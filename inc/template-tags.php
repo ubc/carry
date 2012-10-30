13fe7c81356d8d86fe6bcbba5d0d52c7427f32a8
Enej Bajgoric <enej.bajgoric@gmail.com>
2012-10-30 16:41:26 -0700

    Added Screenshot and update the template tags

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -33,11 +33,11 @@ function carry_content_nav( $nav_id ) {
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
 		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'carry' ) ); ?></div>
+		<div class="nav-previous"><?php next_posts_link( '<i class="icon-chevron-left"></i>'.__( 'Older posts', 'carry' ) ); ?></div>
 		<?php endif; ?>
 
 		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'carry' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'carry' ). '<i class="icon-chevron-right"></i>' ); ?></div>
 		<?php endif; ?>
 
 	<?php endif; ?>
