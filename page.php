80edf777e35c0b8201ba1e588c3f131e195426aa
Devindra Payment <Arden.ived@gmail.com>
2013-05-01 16:00:22 -0700

    Worked on making Carry more mobile friendly.
    It's smoother now, and collapses into a column view, but the left side
    bar is off position for desktop screen sizes, as well, the Pulse CPT
    widget has elements overlapping when the screen is too small.

--- page.php
+++ page.php
@@ -10,24 +10,22 @@
  * @package carry
  * @since carry 1.0
  */
-
-get_header(); ?>
-
-		<div id="primary" class="site-content">
-			<div id="content" role="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php 
-					$carry_content_template = apply_filters( 'carry_content_template', 'content', $post->post_type );
-					get_template_part( $carry_content_template, 'page' ); ?>
-
-					<?php comments_template( '', true ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary .site-content -->
+?>
+<?php get_header(); ?>
 
 <?php get_sidebar(); ?>
+
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+		<?php
+			while ( have_posts() ):
+				the_post();
+				$carry_content_template = apply_filters( 'carry_content_template', 'content', $post->post_type );
+				get_template_part( $carry_content_template, 'page' );
+				comments_template( '', true );
+			endwhile; 
+		?>
+	</div><!-- #content -->
+</div><!-- #primary .site-content -->
+
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -6,44 +6,38 @@
  * @since carry 1.0
  */
 ?>
-		<div id="secondary" class="widget-area" role="complementary">
-		    
-			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-				
-				<aside class="widget widget_nav_menu">
-					<div class="access">
-					  <ul class="menu"><?php wp_nav_menu(); ?></ul>
-					</div>
-				</aside>
+<div id="secondary" class="widget-area" role="complementary">
+	<?php do_action( 'before_sidebar' ); ?>
+	<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+		<aside class="widget widget_nav_menu">
+			<div class="access">
+			  <ul class="menu"><?php wp_nav_menu(); ?></ul>
+			</div>
+		</aside>
+	<?php endif; // end sidebar widget area ?>
+</div><!-- #secondary .widget-area -->
 
-			<?php endif; // end sidebar widget area ?>
-		</div><!-- #secondary .widget-area -->
+<div id="tertiary" class="widget-area" role="complementary">
+	<?php do_action( 'before_sidebar' ); ?>
+	<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
+		<aside id="search" class="widget widget_search">
+			<?php get_search_form(); ?>
+		</aside>
 		
+		<aside id="archives" class="widget">
+			<h1 class="widget-title"><?php _e( 'Archives', 'carry' ); ?></h1>
+			<ul>
+				<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+			</ul>
+		</aside>
 		
-		<div id="tertiary" class="widget-area" role="complementary">
-			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>
-				
-				<aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
-
-				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'carry' ); ?></h1>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
-				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'carry' ); ?></h1>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
-
-			<?php endif; // end sidebar widget area ?>
-		</div><!-- #secondary .widget-area -->
+		<aside id="meta" class="widget">
+			<h1 class="widget-title"><?php _e( 'Meta', 'carry' ); ?></h1>
+			<ul>
+				<?php wp_register(); ?>
+				<li><?php wp_loginout(); ?></li>
+				<?php wp_meta(); ?>
+			</ul>
+		</aside>
+	<?php endif; // end sidebar widget area ?>
+</div><!-- #secondary .widget-area -->
